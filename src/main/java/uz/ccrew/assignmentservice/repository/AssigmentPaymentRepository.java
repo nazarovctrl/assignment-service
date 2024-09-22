3ef19a13aba73cffe8a7046b4f7087580ba241cf
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-22 21:53:04 +0500

    Removed AssigmentPayment

--- src/main/java/uz/ccrew/assignmentservice/repository/AssigmentPaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package uz.ccrew.assignmentservice.repository;
-
-import uz.ccrew.assignmentservice.entity.AssigmentPayment;
-
-public interface AssigmentPaymentRepository extends BasicRepository<AssigmentPayment, Long> {
-}
