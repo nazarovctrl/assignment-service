91d6385b65f5c21cda7d5f681f975563a3238f63
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-23 20:32:58 +0500

    Added NotificationService

--- src/main/java/uz/ccrew/assignmentservice/AssignmentServiceApplication.java
+++ src/main/java/uz/ccrew/assignmentservice/AssignmentServiceApplication.java
@@ -1,9 +1,11 @@
 package uz.ccrew.assignmentservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableAsync
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @SpringBootApplication
 public class AssignmentServiceApplication {
