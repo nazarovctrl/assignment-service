d81a86bc9ba7a7e9cb353da9afd3f42b004a929d
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-23 22:31:39 +0500

    Added Payment service

--- src/test/java/uz/ccrew/assignmentservice/AssignmentServiceApplicationTests.java
+++ src/test/java/uz/ccrew/assignmentservice/AssignmentServiceApplicationTests.java
@@ -1,26 +1,14 @@
 package uz.ccrew.assignmentservice;
 
-import uz.ccrew.assignmentservice.enums.PaymentType;
-import uz.ccrew.assignmentservice.payment.PaymentService;
-import uz.ccrew.assignmentservice.entity.RequisiteAssignment;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AssignmentServiceApplicationTests {
-    @Autowired
-    private PaymentService paymentService;
 
     @Test
     void contextLoads() {
-        paymentService.withdraw(RequisiteAssignment.builder()
-                        .paymentType(PaymentType.CARD)
-                        .cardNumber("8600492939187759")
-                        .paymentAmount(1000L)
-                .build());
     }
 }
