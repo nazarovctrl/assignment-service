f644b6c07f9880484493ef4a02e5ccc7600a4768
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-25 08:29:40 +0500

    Added auto cancellation

--- src/main/java/uz/ccrew/assignmentservice/repository/AssignmentRepository.java
+++ src/main/java/uz/ccrew/assignmentservice/repository/AssignmentRepository.java
@@ -1,10 +1,19 @@
 package uz.ccrew.assignmentservice.repository;
 
 import uz.ccrew.assignmentservice.entity.Assignment;
+import uz.ccrew.assignmentservice.enums.AssignmentStatus;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AssignmentRepository extends BasicRepository<Assignment, Long> {
     Page<Assignment> findAllByCreatedBy_Id(Long userId, Pageable pageable);
+
+    @Override
+    List<Assignment> findAll();
+
+    List<Assignment> findAllByStatusAndModifiedOnLessThan(AssignmentStatus status, LocalDateTime dateTime);
 }
