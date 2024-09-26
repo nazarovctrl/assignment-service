23569c48f6b49a6ee6da379a3d3b74e53dcb17e2
Azimjon Nazarov <109890132+nazarovv2@users.noreply.github.com>
2024-09-26 21:35:34 +0500

    Added Chat tables

---
+++ src/main/java/uz/ccrew/assignmentservice/chat/ChatMessage.java
@@ -0,0 +1,39 @@
+package uz.ccrew.assignmentservice.chat;
+
+import uz.ccrew.assignmentservice.entity.Auditable;
+
+import lombok.*;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "chat_messages")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage extends Auditable {
+    @Id
+    @UuidGenerator
+    private UUID messageId;
+
+    @Column(name = "chat_id", nullable = false)
+    private UUID chatId;
+
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;
+
+    @Column(nullable = false)
+    private String content;
+
+
+    @ManyToOne
+    @JoinColumns(value = {
+            @JoinColumn(name = "chat_id", referencedColumnName = "chat_id", insertable = false, updatable = false),
+            @JoinColumn(name = "sender_id", referencedColumnName = "user_id", insertable = false, updatable = false)
+    }, foreignKey = @ForeignKey(name = "chat_messages_f1"))
+    private ChatUser chatUser;
+}
